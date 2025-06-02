d1db4df78cecc073d6477698485466bfdb4921c9
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-06-02 11:51:56 +0700

    implement get internship programs #16

--- src/app/(role)/school/internship-programs/page.tsx
+++ src/app/(role)/school/internship-programs/page.tsx
@@ -1,17 +1,36 @@
-﻿import React from 'react';
-import type {Metadata} from 'next';
-
-export const metadata: Metadata = {
-  title: "Internship Programs",
-  description: "Manage your school's internship programs on UpHub",
-};
+﻿"use client";
+import React from "react";
+import UInternshipProgramList from "@/features/internship-program/components/UInternshipProgramList";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/shadcn/select";
+import { InternshipProgramSortType } from "@/types/internshipProgram";
+import { useAppDispatch } from "@/libs/rtk/hooks";
+import { setSort } from "@/features/internship-program/slices/internshipProgramSlices";
 
 const InternshipProgramsPage = () => {
+  const dispatch = useAppDispatch();
   return (
-      <div>
-        <h1>Internship Programs</h1>
-        <p>Create, edit, and manage internship programs for your school's students.</p>
+    <div>
+      <div className="flex flex-row justify-between">
+        <h1 className="text-custom-blue-2 text-2xl font-bold mb-5">Internship Programs</h1>
+        {/* Order By */}
+        <div className="flex justify-end pb-2">
+          <Select onValueChange={(value) => dispatch(setSort(value))}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Sắp xếp theo" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value={InternshipProgramSortType.endingSoon}>Sắp kết thúc</SelectItem>
+                <SelectItem value={InternshipProgramSortType.upcoming}>Sắp diễn ra</SelectItem>
+                <SelectItem value={null}>All</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
+
+      <UInternshipProgramList />
+    </div>
   );
 };
 
--- src/libs/rtk/store.tsx
+++ src/libs/rtk/store.tsx
@@ -1,9 +1,11 @@
 import {applicationSlice} from "@/features/application/slices/applicationSlice";
 import {companySlice} from "@/features/company/slices/companySlice";
+import { internshipProgramSlice } from '@/features/internship-program/slices/internshipProgramSlices';
 import {jobSlice} from "@/features/job/slices/jobSlice";
 import {applicationsApi} from "@/services/applicationsApi";
 import { citiesApi } from '@/services/citiesApi';
 import {companiesApi} from "@/services/companiesApi";
+import { internshipProgramsApi } from '@/services/internshipsApi';
 import {jobsApi} from "@/services/jobsApi";
 import {configureStore, Middleware} from "@reduxjs/toolkit";
 
@@ -15,17 +17,20 @@ export function makeStore() {
             [applicationsApi.reducerPath]: applicationsApi.reducer,
             [jobsApi.reducerPath]: jobsApi.reducer,
             [citiesApi.reducerPath]: citiesApi.reducer,
+            [internshipProgramsApi.reducerPath]: internshipProgramsApi.reducer,
 
             jobParams: jobSlice.reducer,
             companyParams: companySlice.reducer,
             applicationParams: applicationSlice.reducer,
+            internshipProgramParams: internshipProgramSlice.reducer
         },
         middleware: (getDefaultMiddleware) =>
                 getDefaultMiddleware()
                 .concat(companiesApi.middleware as Middleware)
                 .concat(applicationsApi.middleware as Middleware)
                 .concat(jobsApi.middleware as Middleware)
-                .concat(citiesApi.middleware as Middleware),
+                .concat(citiesApi.middleware as Middleware)
+                .concat(internshipProgramsApi.middleware as Middleware),
     });
 }
 
