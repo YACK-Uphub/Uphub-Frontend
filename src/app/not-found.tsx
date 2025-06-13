cdf6983a0b45780c65af86383e49109b443e57c0
AnonyFriday <duykimvu75@gmail.com>
2025-06-13 22:30:03 +0700

    Fix errors on SSR #34

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,37 +1,30 @@
-"use client"
-
-import React from 'react';
-import {useRouter} from 'next/navigation';
+// app/not-found/page.tsx
+import Link from 'next/link';
 import type {Metadata} from 'next';
-import UButton from "@/components/shared/UButton";
+import UButton from '@/components/shared/UButton';
 
 export const metadata: Metadata = {
-  title: "Not Found",
-  description: "The page you are looking for does not exist",
+  title: 'Not Found',
+  description: 'The page you are looking for does not exist',
 };
 
-const NotFoundPage = () => {
-
-  const router = useRouter();
-  const goBackToHomePage = () => router.push('/');
-
+export default function NotFoundPage() {
   return (
       <div className="flex h-full flex-col items-center justify-center px-4 bg-custom-white">
-        <h1 className="mb-4 text-4xl font-bold text-custom text-custom-blue-3">
+        <h1 className="mb-4 text-4xl font-bold text-custom-blue-3">
           404 | Trang không tồn tại
         </h1>
         <p className="mb-6 text-custom-black">
           Trang bạn tìm kiếm không tồn tại hoặc đã bị xóa.
         </p>
         <div className="flex gap-4">
-          <UButton
-              onClick={goBackToHomePage}
-              label={"Quay trở lại trang chủ"}
-              backgroundColor={"bg-custom-blue-3"}
-          />
+          <Link href="/">
+            <UButton
+                label="Quay trở lại trang chủ"
+                backgroundColor="bg-custom-blue-3"
+            />
+          </Link>
         </div>
       </div>
   );
-};
-
-export default NotFoundPage;
+}
